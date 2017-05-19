7d1b211e59f59e90bb04c27a60b08b918c93229a
Isaac <isaacxpreston@gmail.com>
2017-05-19 15:23:25 -0400

    background rollovers

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 riot.tag2('highlighter', '<div class="highlighter"> <img riot-src="{opts.img}"> <h1 class="fonts header-2">{opts.title}</h1> </div>', '', '', function(opts) {
 });
-riot.tag2('linkbox', '<a href="{opts.url}"> <div class="footer-image"> <div class="label fonts utility"><p>{opts.text}</p></div> </div> </a>', '', '', function(opts) {
+riot.tag2('linkbox', '<a href="{opts.url}"> <div class="footer-image"> <div class="footer-background" riot-style="background: url(\'{opts.img}\'); background-size: cover; background-position: center center; background-repeat: no-repeat;"></div> <div class="label fonts utility"><p>{opts.text}</p></div> </div> </a>', '', '', function(opts) {
+    console.log(opts.img)
+    console.log(opts.text)
 });
--- scripts/tags/linkbox.tag
+++ scripts/tags/linkbox.tag
@@ -1,7 +1,15 @@
 <linkbox>
   <a href="{opts.url}">
     <div class="footer-image">
+      <div class="footer-background" style="background: url('{opts.img}');
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;"></div>
       <div class="label fonts utility"><p>{opts.text}</p></div>
     </div>
   </a>
+  <script>
+    console.log(opts.img)
+    console.log(opts.text)
+  </script>
 </linkbox>
